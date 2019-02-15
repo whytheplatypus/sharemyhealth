7003ac2be0dcc651429d3a4611cc80342f9fc194
Alan Viars <aviars@videntity.com>
2019-02-15 11:38:42 -0500

    user profile

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,7 +2,11 @@
 
 # Copyright Videntity Systems, Inc.
 from django.conf.urls import url
-from .views.core import (account_settings, )
+from .views.core import account_settings
+from .views.user_profile import oidc_userprofile, oidc_userprofile_test
 urlpatterns = [
     url(r'^settings', account_settings, name='account_settings'),
+    url(r'^userprofile-test', oidc_userprofile_test, name='user_profile_test'),
+    url(r'^userprofile', oidc_userprofile, name='user_profile'),
+
 ]
